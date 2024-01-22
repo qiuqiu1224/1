600e97c340e97d24a0d7b80288a3fc1d4725351f
qll <1045480695@qq.com>
2024-01-22 13:31:07 +0800

    添加项目文件。

---
+++ 01字符串的练习/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01字符串的练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
